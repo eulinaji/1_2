f282e07608df0474a4494f7ebfab39452f9ba005
eulinaji <91710769+eulinaji@users.noreply.github.com>
2021-11-14 21:58:46 -0800

    Update leaderboard.py

--- 1.2.1/leaderboard.py
+++ 1.2.1/leaderboard.py
@@ -19,59 +19,71 @@ def load_leaderboard(file_name, leader_names, leader_scores):
     index = 0
 
     # TODO 1: use a while loop to read the leader name from the line (format is "leader_name,leader_score")
+    while (line[index] != ","):
+      leader_name = leader_name + line[index] 
+      index = index + 1
 
+    print("leader name is: ", leader_name)
 
     # TODO 2: add the leader name to the list
+    leader_names.append(leader_name)
 
-    
     # TODO 3: read the player score using a similar loop
+    index = index + 1
+    while(line[index] != "\n"):
+      leader_score = leader_score + line[index]
+      index = index + 1
 
-    
+    print("leader score is: ", leader_score)    
+
     # TODO 4: add the player score to the list
-
+    leader_scores.append(int(leader_score))
 
   leaderboard_file.close()
-
 
 # update leaderboard by inserting the current player and score to the list at the correct position
 def update_leaderboard(file_name, leader_names, leader_scores, player_name, player_score):
 
   leader_index = 0
   # TODO 5: loop through all the scores in the existing leaderboard list
-  '''
-    while ():
+  
+  while (leader_index < len(leader_scores)):
     # TODO 6: check if this is the position to insert new score at
-    if ():
+    if (player_score >= leader_scores[leader_index]):
       break
     else:
       leader_index = leader_index + 1
-  '''
+  
   # TODO 7: insert the new player and score at the appropriate position
-
+    leader_scores.insert(leader_index, player_score)
+    leader_names.insert(leader_index, player_name)
 
   # TODO 8: keep both lists at 5 elements only (top 5 players)
-
+    if (len(leader_names) == 6):
+      leader_names.pop(5)
+    if (len(leader_scores) == 6):
+      leader_scores.pop(5)
   
   # store the latest leaderboard back in the file
-  leaderboard_file = open(file_name, "w")  # this mode opens the file and erases its contents for a fresh start
-  leader_index = 0
+    leaderboard_file = open(file_name, "w")  # this mode opens the file and erases its contents for a fresh start
+    leader_index = 0
   # TODO 9: loop through all the leaderboard elements and write them to the file
-  '''
-    while ():
-    leaderboard_file.write(leader_names[leader_index] + "," + str(leader_scores[leader_index]) + "\n")
-    leader_index = leader_index + 1
-  '''
-  leaderboard_file.close()
   
+    while (leader_index < len(leader_names)):
+      leaderboard_file.write(leader_names[leader_index] + "," + str(leader_scores[leader_index]) + "\n")
+      leader_index = leader_index + 1
+  
+    leaderboard_file.close()
 
 # draw leaderboard and display a message to player
 def draw_leaderboard(leader_names, leader_scores, high_scorer, turtle_object, player_score):
   
   # clear the screen and move turtle object to (-200, 100) to start drawing the leaderboard
-  font_setup = ("Arial", 20, "normal")
+  turtle_object.color("lightskyblue")
+  font_setup = ("System", 10, "normal")
   turtle_object.clear()
   turtle_object.penup()
-  turtle_object.goto(-200,100)
+  turtle_object.goto(-200,50)
   turtle_object.hideturtle()
   turtle_object.down()
   leader_index = 0
@@ -80,7 +92,7 @@ def draw_leaderboard(leader_names, leader_scores, high_scorer, turtle_object, pl
   while leader_index < len(leader_names):
     turtle_object.write(str(leader_index + 1) + "\t" + leader_names[leader_index] + "\t" + str(leader_scores[leader_index]), font=font_setup)
     turtle_object.penup()
-    turtle_object.goto(-200,int(turtle_object.ycor())-50)
+    turtle_object.goto(-200,int(turtle_object.ycor())-20)
     turtle_object.down()
     leader_index = leader_index + 1
 
@@ -102,4 +114,3 @@ def draw_leaderboard(leader_names, leader_scores, high_scorer, turtle_object, pl
     turtle_object.write("You earned a silver medal!", font=font_setup)
     turtle_object.write("You earned a gold medal!", font=font_setup)
   '''
-  
