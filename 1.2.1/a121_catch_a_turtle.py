778cca4d6b2d6d3a7d544e8c61479201b402b3ff
eulinaji <91710769+eulinaji@users.noreply.github.com>
2021-11-14 21:59:23 -0800

    Update a121_catch_a_turtle.py

--- 1.2.1/a121_catch_a_turtle.py
+++ 1.2.1/a121_catch_a_turtle.py
@@ -5,39 +5,40 @@ import random as rand
 import leaderboard as lb
 
 #-----game configuration----
-colors = ["black", "sky blue", "salmon", "orchid", "pale green"]
-spot_color = "salmon"
-spot_shape = "square"
+colors = ["lightskyblue", "palegreen", "peachpuff", "plum"]
+spot_color = "lightskyblue"
+spot_shape = "turtle"
 spot_size = 2
 
-font_setup = ("System", 20, "normal")
+font_setup = ("System", 20, "bold")
 
 score = 0
 
-timer = 5
+timer = 30
 counter_interval = 1000
 timer_up = False
 
-leaderboard_file_name = "a122_leaderboard.txt"
+leaderboard_file_name = "leaderboard.txt"
 leader_names_list = []
 leader_scores_list = []
-player_name = input ("Please enter your name:")
+name = input ("Your name: ")
 
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.fillcolor(spot_color)
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
+spot.pencolor(spot_color)
 
 score_writer = trtl.Turtle()
 score_writer.hideturtle()
 score_writer.penup()
-score_writer.goto(-250,200)
+score_writer.goto(-200,120)
 
-counter =  trtl.Turtle()
+counter = trtl.Turtle()
 counter.hideturtle()
 counter.penup()
-counter.goto(150,200)
+counter.goto(50,120)
 
 #-----game functions--------
 def spot_clicked(x,y):
@@ -49,10 +50,10 @@ def spot_clicked(x,y):
         spot.hideturtle()
 
 def change_position():
-    leave_mark()
     resize()
-    new_xpos = rand.randint(-300,300)
-    new_ypos = rand.randint(-300,300)
+    recolor()
+    new_xpos = rand.randint(-200,200)
+    new_ypos = rand.randint(-150,80)
     spot.penup()
     spot.hideturtle()
     spot.goto(new_xpos,new_ypos)
@@ -61,34 +62,38 @@ def change_position():
 def update_score():
     global score
     score = score + 1
-    score_writer.clear()  
-    score_writer.write(score, font=font_setup)
+    score_writer.clear() 
+    score_writer.color("white") 
+    score_writer.write("Score: " + str(score), font=font_setup)
 
 def countdown():
   global timer, timer_up
   counter.clear()
+  counter.color("white") 
   if timer <= 0:
-    counter.write("Time's Up", font=font_setup)
+    counter.write("Time's Up!", font=font_setup)
     timer_up = True
+    spot.hideturtle()
     manage_leaderboard()
-
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer = timer - 1
     counter.getscreen().ontimer(countdown, counter_interval)
 
+
 def resize():
-    sizes = [0.5, 1, 1.5, 2]
+    sizes = [1, 1.5, 2, 2.5, 3, 3.5, 4]
     spot.shapesize(rand.choice(sizes))
 
-def leave_mark():
-    spot.fillcolor(rand.choice(colors[1:]))
-    spot.stamp()
-    spot.fillcolor(colors[0])
+def recolor():
+    new_color = rand.choice(colors[0:])
+    spot.fillcolor(new_color)
+    spot.pencolor(new_color)
 
 def start_game():
     spot.onclick(spot_clicked)
     counter.getscreen().ontimer(countdown, counter_interval)
+
 
 def manage_leaderboard():
     global leader_scores_list
@@ -98,14 +103,15 @@ def manage_leaderboard():
     lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
 
     if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
-        lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+        lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, name, score)
         lb.draw_leaderboard(leader_names_list, leader_scores_list, True, spot, score)
     else:
         lb.draw_leaderboard(leader_names_list, leader_scores_list, False, spot, score)
 
+
 #-----events----------------
 wn = trtl.Screen()
-wn.bgcolor("peachpuff")
+wn.bgcolor("black")
 spot.onclick(spot_clicked)
-# wn.ontimer(countdown, counter_interval) 
+wn.ontimer(countdown, counter_interval) 
 wn.mainloop()
